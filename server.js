4f659024f6f74b32c60f6f43c0e17490f455f655
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-04-13 10:45:37 +0530

    Added middlewares

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import testRoutes from './routes/testRoutes.js'
+import cors from 'cors';
+import morgan from 'morgan';
 
 // configuring dotenv
 dotenv.config()
@@ -15,10 +18,17 @@ connectDB()
 // rest object
 const app = express();
 
+// Middleware to tell application that we are dealing with json data
+app.use(express.json());
+
+// Middleware to connect frontend and backend otherwise will get origin error as we are dealing with different ports
+app.use(cors());
+
+// Middleware to know which api gets hitted and how much time it took.
+app.use(morgan('dev'));
+
 // route
-app.get('/',(req,resp)=>{
-    resp.send("<h1>Welcome to job finder</h1>");
-})
+app.use('/api/v1/test',testRoutes)
 
 // Port
 const PORT = process.env.PORT || 8080;
