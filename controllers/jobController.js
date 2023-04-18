79e0456d71e4997102f7a5866e7cecf6c6da2553
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-04-18 16:20:56 +0530

    created route and controller for job filters

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,6 @@
 import jobsModel from "../models/jobsModel.js";
+import mongoose from "mongoose";
+import moment from "moment";
 
 export const createJobController = async (req,resp,next) =>{
     const {company,position} = req.body;
@@ -58,4 +60,60 @@ export const deleteJobController = async (req,resp,next)=>{
     resp.status(200).json({
         message:"Success, Job deleted"
     })
+}
+
+export const jobStatsController = async (req,resp)=>{
+    const stats = await jobsModel.aggregate([
+        // search by user jobs
+        {
+            $match:{
+                createdBy:new mongoose.Types.ObjectId(req.user.userId),
+            },
+        },
+        {
+            $group:{
+                _id:'$status',count:{$sum:1}
+            }
+        }
+    ])
+    //default stats
+    const defaultStats = {
+        pending: stats.pending || 0,
+        reject: stats.reject || 0,
+        interview: stats.interview || 0,
+    };
+
+    //monthly yearly stats
+    let monthlyApplication = await jobsModel.aggregate([
+        {
+            $match: {
+                createdBy: new mongoose.Types.ObjectId(req.user.userId),
+            },
+        },
+        {
+            $group: {
+                _id: {
+                    year: { $year: "$createdAt" },
+                    month: { $month: "$createdAt" },
+                },
+                count: {
+                    $sum: 1,
+                },
+            },
+        },
+    ]);
+    monthlyApplication = monthlyApplication.map((item) => {
+            const {
+                _id: { year, month },
+                count,
+            } = item;
+            const date = moment()
+                .month(month - 1)
+                .year(year)
+                .format("MMM Y");
+            return { date, count };
+        }
+    ).reverse();
+
+    resp.status(200).json({totalJob:stats.length ,stats,defaultStats,monthlyApplication});
 }
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import userAuth from './../middlewares/authMiddleware.js';
-import { createJobController, deleteJobController, getAllJobsController, updateJobController } from '../controllers/jobController.js';
+import { createJobController, deleteJobController, getAllJobsController, jobStatsController, updateJobController } from '../controllers/jobController.js';
 
 const router = express.Router()
 
@@ -9,5 +9,6 @@ router.post('/create-job',userAuth,createJobController);
 router.get('/get-job',userAuth,getAllJobsController);
 router.patch('/update-job/:id',userAuth,updateJobController);
 router.delete('/delete-job/:id',userAuth,deleteJobController);
+router.get('/job-stats',userAuth,jobStatsController)
 
 export default router;
