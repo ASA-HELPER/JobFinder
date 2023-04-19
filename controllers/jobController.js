147159529feffcfb46bee07aea4d86cda69ae789
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-04-19 10:45:53 +0530

    Created logic for pagination

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -57,13 +57,22 @@ export const getAllJobsController = async (req,resp,next)=>{
         queryResult = queryResult.sort('-position');
     }
 
+    // pagination filter
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1)*limit;
+    queryResult = queryResult.skip(skip).limit(limit)
+    const totalJobs = await jobsModel.countDocuments(queryResult)
+    const numOfPage = Math.ceil(totalJobs/limit)
+
 
     const jobs = await queryResult;
 
     // const jobs = await jobsModel.find({createdBy:req.user.userId});
     resp.status(200).json({
-        totalJobs : jobs.length,
-        jobs
+        totalJobs,
+        jobs,
+        numOfPage
     })
 }
 
