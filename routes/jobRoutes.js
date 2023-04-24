3076eb275be8bc3ea3364262804ce2c1742d5833
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-04-24 15:58:54 +0530

    Created swagger api schema structure for job routes

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -4,6 +4,49 @@ import { createJobController, deleteJobController, getAllJobsController, jobStat
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Job:
+ *      type: object
+ *      required:
+ *        - company
+ *        - position
+ *        - workLocation
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: The Auto-generated id of user collection
+ *          example : DHSASDHJDJHVAJDSVJAVSD
+ *        company:
+ *          type: string
+ *          description: Company name
+ *        position:
+ *          type: string
+ *          description: Job Position name (upto 100 characters)
+ *        status:
+ *          type: string
+ *          description: Job Status (pending,reject,interview)
+ *        workType:
+ *          type: string
+ *          description: Type of hiring (full-time,part-time,internship,contract)
+ *        workLocation:
+ *          type: string
+ *          description: Work location city or country
+ *        createdBy:
+ *          type: ObjectId
+ *          description: Job creator Id
+ *      example:
+ *        id: GDHJGD788BJBJ
+ *        company: Google
+ *        position: Software Developer
+ *        status: interview
+ *        workType: full-time
+ *        workLocation: Silicon Valley
+ *        createdBy: B237I389B24f82401
+ */
+
 // routes
 router.post('/create-job',userAuth,createJobController);
 router.get('/get-job',userAuth,getAllJobsController);
