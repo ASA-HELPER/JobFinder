f730452eb5e85bb8251aacdb7ae8df27bdffddbc
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2023-04-29 12:12:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const options = {
         },
         servers:[
             {
-                url:"http://localhost:8080"
+                url:"https://jobfinder-5wbv.onrender.com/"
             }
         ],
     },
